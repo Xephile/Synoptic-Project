1dab25e27a9a95f3a3c313e8ee998cbbac8613b4
Xephile <dean.munday@hotmail.co.uk>
2022-11-01 17:03:09 +0000

    fix? hrefs in footer

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -4,21 +4,21 @@ const Footer = () => {
 
       <div className="d-flex flex-column">
         <section className="w-25">
-          <a>
+          <a href="https://wonderful-mccarthy-490783.netlify.app">
             <img
               src="https://img.icons8.com/nolan/344/facebook.png"
               alt="facebook"
               className="w-25 me-2"
             />
           </a>
-          <a>
+          <a href="https://wonderful-mccarthy-490783.netlify.app">
             <img
               src="https://img.icons8.com/nolan/344/twitter.png"
               alt="twitter"
               className="w-25 mx-2"
             />
           </a>
-          <a>
+          <a href="https://wonderful-mccarthy-490783.netlify.app">
             <img
               src="https://img.icons8.com/nolan/344/instagram-new.png"
               alt="instagram"
@@ -27,39 +27,39 @@ const Footer = () => {
           </a>
         </section>
         <h4 className="py-1">
-          <a className="text-white">
+          <a href="https://wonderful-mccarthy-490783.netlify.app" className="text-white">
             Terms and Conditions
           </a>
         </h4>
         <h4 className="py-1">
-          <a className="text-white">
+          <a href="https://wonderful-mccarthy-490783.netlify.app" className="text-white">
             FAQ's
           </a>
         </h4>
         <h4 className="py-1">
-          <a className="text-white">
+          <a href="https://wonderful-mccarthy-490783.netlify.app" className="text-white">
             Forums
           </a>
         </h4>
       </div>
       <div className="d-flex flex-column justify-content-around">
         <h4>
-          <a className="text-white">
+          <a href="https://wonderful-mccarthy-490783.netlify.app" className="text-white">
             Contrabuting
           </a>
         </h4>
         <h4>
-          <a className="text-white">
+          <a href="https://wonderful-mccarthy-490783.netlify.app" className="text-white">
             Acknowledgements
           </a>
         </h4>
         <h4>
-          <a className="text-white">
+          <a href="https://wonderful-mccarthy-490783.netlify.app" className="text-white">
             Blog
           </a>
         </h4>
         <h4>
-          <a className="text-white">
+          <a href="https://wonderful-mccarthy-490783.netlify.app" className="text-white">
             Privacy
           </a>
         </h4>
